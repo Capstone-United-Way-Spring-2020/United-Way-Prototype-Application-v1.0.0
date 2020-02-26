7ffabc1f9f62d4e82feefdb8db6478e9a771a8c5
Christian <ctgolczynski@crimson.ua.edu>
2020-02-25 18:41:22 -0600

    Agency overview/create agency are LIVE

---
+++ DataLibrary/Models/AgencyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    //this is a front end, user interface model and is different from the data access model
+    //dont want to decorate these models, not needed (name, requirements, error messages etc)
+    //DATABASE DOESNT CARE about modifiers, only the properties themselves (below)
+    public class AgencyModel
+    {
+        public int AgencyID { get; set; }
+
+        public string AgencyName { get; set; }
+
+        public bool AgencyStatus { get; set; } = true;
+
+        public DateTime AgencyDateCreated { get; set; } = DateTime.Now;
+
+        public DateTime AgencyDateLastEdited { get; set; }
+    }
+}
--- UnitedWayPrototypeApplication/Controllers/HomeController.cs
+++ UnitedWayPrototypeApplication/Controllers/HomeController.cs
@@ -35,11 +35,11 @@ namespace UnitedWayPrototypeApplication.Controllers
         {
             ViewBag.Message = "Employee Overview";
 
-            //using the SQL SELECT statements in EmployeeProcessor to LOAD the employees
+            //using the SQL SELECT statements in EmployeeProcessor to LOAD the employees to a list
             var data = DataLibrary.BusinessLogic.EmployeeProcessor.LoadEmployees();
             List<EmployeeModel> employees = new List<EmployeeModel>();
 
-            //displaying all the loaded employees
+            // creating new row for each record
             foreach (var row in data)
             {
                 employees.Add(new EmployeeModel
@@ -92,47 +92,48 @@ namespace UnitedWayPrototypeApplication.Controllers
             return View();
         }
 
-
         public ActionResult Agency()
         {
             ViewBag.Message = "Agency Overview";
+            //utilizing the SQL SELECT statements in AgencyProcessor to LOAD the agencies
+            var data = DataLibrary.BusinessLogic.AgencyProcessor.LoadAgencies();
 
-            List<Models.AgencyModel> agencies = new List<Models.AgencyModel>();
-
-            agencies.Add(new Models.AgencyModel { AgencyId = "001", AgencyName = "2-1-1/Information & Referral" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "002", AgencyName = "Alabama Head Injury Foundation" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "003", AgencyName = "American Red Cross" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "004", AgencyName = "Arts 'n Autism" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "005", AgencyName = "Big Brothers Big Sisters of West Alabama" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "006", AgencyName = "Black Warrior Council Boy Scouts" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "007", AgencyName = "Boys & Girls Clubs of West Alabama" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "008", AgencyName = "Caring Days Adult Day Care" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "009", AgencyName = "Child Abuse Prevention Services" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "010", AgencyName = "Easterseals West Alabama" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "011", AgencyName = "Family Counseling Service" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "012", AgencyName = "Five Horizons Health Services" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "013", AgencyName = "FOCUS on Senior Citizens" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "014", AgencyName = "Girl Scouts of North-Central Alabama" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "015", AgencyName = "Good Samaritan Clinic" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "016", AgencyName = "Hospice of West Alabama" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "017", AgencyName = "Phoenix House" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "018", AgencyName = "Sickle Cell Disease Association" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "019", AgencyName = "Success By 6" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "020", AgencyName = "Temporary Emergency Services" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "021", AgencyName = "The Arc of Tuscaloosa Country" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "022", AgencyName = "The Salvation Army" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "023", AgencyName = "Turning Point" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "024", AgencyName = "Tuscaloosa's One Place" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "025", AgencyName = "United Cerebral Palsy of West Alabama" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "026", AgencyName = "United Way of West Alabama" });
-            agencies.Add(new Models.AgencyModel { AgencyId = "027", AgencyName = "YMCA" });
-
+            //using the SQL SELECT statements in AgencyProcessor to LOAD the agencies to a list
+            List<AgencyModel> agencies = new List<AgencyModel>();
+            //create new row for each record
+            foreach (var row in data)
+            {
+                agencies.Add(new AgencyModel
+                {
+                    AgencyID = row.AgencyID,
+                    AgencyName = row.AgencyName,
+                    AgencyStatus = row.AgencyStatus,
+                    AgencyDateCreated = row.AgencyDateCreated,
+                    AgencyDateLastEdited = row.AgencyDateLastEdited
+                });
+            }
+            
             return View(agencies);
         }
 
         public ActionResult CreateAgency()
         {
             ViewBag.Message = "Create new Agency";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateAgency(AgencyModel model)
+        {
+            ViewBag.Message = "Create new Agency";
+            
+            if (ModelState.IsValid)
+            {
+                DataLibrary.BusinessLogic.AgencyProcessor.CreateAgency(model.AgencyID, model.AgencyName, model.AgencyStatus, model.AgencyDateCreated, model.AgencyDateLastEdited);
+            }
+
 
             return View();
         }
