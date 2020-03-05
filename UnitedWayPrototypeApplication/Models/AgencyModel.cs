0074fee0be9735683becead57647d345107ff1f6
MDay18 <mbday@crimson.ua.edu>
2020-03-05 16:20:52 -0600

    Agency Model Data Validation Edits.

--- UnitedWayPrototypeApplication/Models/AgencyModel.cs
+++ UnitedWayPrototypeApplication/Models/AgencyModel.cs
@@ -9,20 +9,26 @@ namespace UnitedWayPrototypeApplication.Models
     public class AgencyModel
     {
         [Display(Name = "Agency ID")]
-        [Required(ErrorMessage = "You must enter an Agency ID")]
+        [Range(0, 100, ErrorMessage = "Please enter a valid Agency ID.")]
+        [Required(ErrorMessage = "You must enter an Agency ID.")]
         public int AgencyID { get; set; }
 
         [Display(Name = "Agency Name")]
-        [Required(ErrorMessage = "You must enter an Agency Name")]
+        [Required(ErrorMessage = "You must enter an Agency Name.")]
+        [MinLength(1)]
         public string AgencyName { get; set; }
 
+        // Commented out in Views file
         [Display(Name = "Agency Status")]
         public bool AgencyStatus { get; set; } = true;
 
+        // Commented out in Views file
         [Display(Name = "Agency Date Created")]
         public DateTime AgencyDateCreated { get; set; } = DateTime.Now;
 
+        // Commented out in Views file
         [Display(Name = "Agency Last Edited")]
         public DateTime AgencyDateLastEdited { get; set; } = DateTime.Now;
     }
+
 }
